e4a5de113696c10b754561540ce8a3c70fe02adf
mjsimo99 <mohashark21@gmail.com>
2023-12-20 09:56:30 +0100

    git commit -m "model mapper"

--- src/main/java/com/satoru/satofish/modelmapper/AppConfig.java
+++ src/main/java/com/satoru/satofish/modelmapper/AppConfig.java
@@ -1,6 +1,7 @@
 package com.satoru.satofish.modelmapper;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+
         return new ModelMapper();
     }
 }
